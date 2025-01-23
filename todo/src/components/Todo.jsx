5f5b138c42868419f918c1da0039b649bbf9d3c5
kamleshyad <kamleshyadav1795@gmail.com>
2025-01-23 19:00:04 +0530

    suggestion css done

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -1,23 +1,23 @@
-import { useReducer, useState, useEffect } from "react";
-import Swal from "sweetalert2";
+import Swal from 'sweetalert2';
+import { useState, useReducer, useEffect} from 'react';
 
 import { TaskList } from "./TaskList";
 
-const initialState = JSON.parse(localStorage.getItem("tasks"))?? [];
+const initialState = JSON.parse(localStorage.getItem('tasks')) ?? [];
 
 const reducer = (state, action) => {
-    switch(action.type) {
+    switch(action.type){
         case "Add-Task" :
             return [...state, handleAddtask(action.payload)]
-        default:
+        default :
             return state;
     }
 }
 
-const handleAddtask = (task) => {
-    return {
+const handleAddtask = (taskName) => {
+    return{
         id : Date.now(),
-        taskName : task,
+        taskName : taskName,
         status : false
     }
 }
@@ -25,64 +25,64 @@ const handleAddtask = (task) => {
 export const Todo = () => {
 
     const [state, dispatch] = useReducer(reducer, initialState);
-    const [taskName, setTaskName] = useState("");
+    const [taskName, setTaskname] = useState("");
     const [suggestion, setSuggestion] = useState([]);
 
     useEffect(() => {
-        localStorage.setItem("tasks" , JSON.stringify(state))
-    }, [state]);
+        localStorage.setItem("tasks", JSON.stringify(state))
+    }, [state])
 
     const handleInputChange = (e) => {
         const input = e.target.value;
-        setTaskName(input);
-    
-        if (input.trim() !== "") {
-            const filteredSuggestions = state
-                .filter((task) =>
-                    task.taskName.toLowerCase().includes(input.toLowerCase())
+        setTaskname(input);
+
+        if(input.trim() !== ""){
+            const filteredSuggestions = state.filter((tasks) => {
+                return(
+                    tasks.taskName.toLowerCase().includes(input.toLowerCase())
                 )
-                .map((task) => task.taskName);
+            }).map((task) => task.taskName)
             setSuggestion(filteredSuggestions);
         } else {
             setSuggestion([]);
         }
-    };
-    
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        const trimmedTaskname = taskName.trim();
-
-        if(trimmedTaskname === ""){
+        const trimmedTask = taskName.trim();
+        
+        if(trimmedTask === ""){
             Swal.fire({
                 icon: "error",
                 title: "Oops...",
                 text: "Task Name cannot be empty!",
-            });
+            })
             return;
         }
 
-        const taskExits = state.some((task) => {
-            return (
-                task.taskName.toLowerCase() === trimmedTaskname.toLowerCase()
+        const taskExits = state.some((tasks) => {
+            return(
+                tasks.taskName.toLowerCase() === trimmedTask.toLocaleLowerCase()
             )
         })
-
         if(taskExits){
             Swal.fire({
                 icon: "error",
                 title: "Oops...",
                 text: "Task Name already Exists !",
             });
-            return setTaskName("");
+            return setTaskname("");
         }
 
-        dispatch({type : "Add-Task", payload : taskName})
+
+        dispatch({type: "Add-Task", payload : taskName});
         Swal.fire({
             icon: "success",
             text: "Task Name added Successfully!",
         });
-        setTaskName("");
+        setTaskname("");
     }
 
     return (
@@ -95,13 +95,15 @@ export const Todo = () => {
                             <li>
                                 <input type="text" placeholder='Add Task' value={taskName} onChange={handleInputChange}/>
                                 {suggestion.length > 0 && (
-                                    <ul className="suggestion-box reset">
+                                    <div className="suggestionsec">
+                                        <ul className=" reset">
                                         {suggestion.map((suggestions, index) => (
-                                            <li key={index} onClick={() => setTaskName(suggestions) }>
+                                            <li key={index} onClick={() => setTaskname(suggestions) }>
                                                 {suggestions}
                                             </li>
                                         ))}
                                     </ul>
+                                    </div>
                                 )}
                             </li>
                             <li>
